aaaab901704e7d706cd0d4c7edd0d358f40bec99
hakushin <hanssy130@gmail.com>
2025-03-22 17:19:12 -0700

    update shuffle function

--- app/stage/page.tsx
+++ app/stage/page.tsx
@@ -21,7 +21,7 @@ const idolShowcase = [
 ];
 
 
-function shuffleArray(array) {
+function shuffleArray<T>(array: T[]): T[] {
   const shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
